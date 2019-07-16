32af6b74aab8f2737a9e2a2a4c5bf48c6c5623cc
Jan Schulte <j.schulte@52north.org>
2019-07-16 18:15:09 +0200

    adjust marker und leaflet clustering

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { registerLocaleData } from '@angular/common';
 import localeDe from '@angular/common/locales/de';
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { Icon } from 'leaflet';
 
 @Component({
   selector: 'app-root',
@@ -18,6 +19,16 @@ export class AppComponent {
 
     // necessary to load information on e.g. what 'medium' date format should look like in German etc.
     registerLocaleData(localeDe);
+
+    // L.Icon.Default.prototype.options
+
+    Icon.Default.mergeOptions({
+      iconRetinaUrl: './assets/images/marker@2x.png',
+      iconUrl: './assets/images/marker.png',
+      shadowUrl: '',
+      iconSize: [32, 32],
+      iconAnchor: [16, 32]
+    });
   }
 
 }
