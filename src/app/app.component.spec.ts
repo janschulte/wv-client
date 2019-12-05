eb4661115570885168c8a772c294f3d6b7190182
Jan Schulte <j.schulte@52north.org>
2019-12-05 13:27:31 +0100

    change app name and remove the new

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'wv-client-new'`, () => {
+  it(`should have as title 'wv-client'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('wv-client-new');
+    expect(app.title).toEqual('wv-client');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to wv-client-new!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to wv-client!');
   });
 });
