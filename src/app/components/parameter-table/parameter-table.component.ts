9267f6ea2b403c026a8dc4b21dc43b592aab9822
Jan Schulte <j.schulte@52north.org>
2020-03-30 15:19:38 +0200

    add textual filter in list selection

--- src/app/components/parameter-table/parameter-table.component.ts
+++ src/app/components/parameter-table/parameter-table.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, SimpleChanges } from '@angular/core';
 import { MultiServiceFilterSelectorComponent } from '@helgoland/selector';
 
 @Component({
@@ -6,4 +6,12 @@ import { MultiServiceFilterSelectorComponent } from '@helgoland/selector';
   templateUrl: './parameter-table.component.html',
   styleUrls: ['./parameter-table.component.scss']
 })
-export class ParameterTableComponent extends MultiServiceFilterSelectorComponent { }
+export class ParameterTableComponent extends MultiServiceFilterSelectorComponent implements OnChanges {
+
+  public textualFilter = '';
+
+  public ngOnChanges(changes: SimpleChanges) {
+    super.ngOnChanges(changes);
+  }
+
+}
