f992f06e6c2c7c81b709dfb3a5cf9a02f4c3317c
Jan Schulte <j.schulte@52north.org>
2020-09-24 15:55:03 +0200

    adjust to newest tb version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { registerLocaleData } from '@angular/common';
 import localeDe from '@angular/common/locales/de';
 import { Component } from '@angular/core';
 import { Settings, SettingsService } from '@helgoland/core';
-import { D3TimeFormatLocaleService } from '@helgoland/d3';
 import { TranslateService } from '@ngx-translate/core';
 import { Icon } from 'leaflet';
 
@@ -18,13 +17,10 @@ export class AppComponent {
   constructor(
     private translate: TranslateService,
     private settings: SettingsService<Settings>,
-    private stateHandler: StateHandlerService,
-    private d3translate: D3TimeFormatLocaleService
+    private stateHandler: StateHandlerService
   ) {
 
     this.setLanguage();
-
-    this.addD3Language();
 
     // necessary to load information on e.g. what 'medium' date format should look like in German etc.
     registerLocaleData(localeDe);
@@ -58,18 +54,4 @@ export class AppComponent {
     }
   }
 
-  private addD3Language() {
-    this.d3translate.addTimeFormatLocale('de',
-      {
-        dateTime: '%a %b %e %X %Y',
-        date: '%d-%m-%Y',
-        time: '%H:%M:%S',
-        periods: ['AM', 'PM'],
-        days: ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag'],
-        shortDays: ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'],
-        months: ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'],
-        shortMonths: ['Jan', 'Feb', 'Mär', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dez']
-      }
-    );
-  }
 }
