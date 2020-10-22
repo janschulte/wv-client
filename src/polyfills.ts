2154dbe890818ec8d04046c0cfbecc70c3a76ae2
Jan Schulte <j.schulte@52north.org>
2020-10-22 12:16:36 +0200

    fix issues with the ie11

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -22,6 +22,9 @@ import '@angular/localize/init';
  * BROWSER POLYFILLS
  */
 
+/** IE 11 needs */
+import 'core-js';
+
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
 
