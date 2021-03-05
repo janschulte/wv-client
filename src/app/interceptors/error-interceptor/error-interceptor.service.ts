2d879ba3b8a7252edd779c516fcb60c49ba42cb9
Jan Schulte <j.schulte@52north.org>
2021-03-05 10:19:00 +0100

    disable logging every request

--- src/app/interceptors/error-interceptor/error-interceptor.service.ts
+++ src/app/interceptors/error-interceptor/error-interceptor.service.ts
@@ -18,9 +18,9 @@ export class ErrorInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(
       map((event: HttpEvent<any>) => {
-        if (event instanceof HttpResponse) {
-          console.log('Http-Response --->>>', event);
-        }
+        // if (event instanceof HttpResponse) {
+        //   console.log('Http-Response --->>>', event);
+        // }
         return event;
       }),
       catchError((error: HttpErrorResponse) => {
